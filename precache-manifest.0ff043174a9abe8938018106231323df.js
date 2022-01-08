a960f54d2d187b3778884792ed8b21ef5dd4b553
vaibhavboliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-01-08 12:45:21 +0530

    Updates

---
+++ precache-manifest.0ff043174a9abe8938018106231323df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "849444e73b207a9c6b72",
+    "url": "/colors-app/static/css/main.08ebb639.chunk.css"
+  },
+  {
+    "revision": "849444e73b207a9c6b72",
+    "url": "/colors-app/static/js/main.69d6c597.chunk.js"
+  },
+  {
+    "revision": "658f8f8a04f3c7d8f86e",
+    "url": "/colors-app/static/js/runtime~main.7b7bc380.js"
+  },
+  {
+    "revision": "5f4339b12058c9c3bcac",
+    "url": "/colors-app/static/css/2.fe2797cd.chunk.css"
+  },
+  {
+    "revision": "5f4339b12058c9c3bcac",
+    "url": "/colors-app/static/js/2.73098f51.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colors-app/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "2c5dbc4ad1d0f2a6cec2a4663ffd2d09",
+    "url": "/colors-app/index.html"
+  }
+];
